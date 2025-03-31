bbeeb0099113160858d83d681ec0cceb584086d8
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2025-04-01 00:11:10 +0100

    switched to native textarea element

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,6 +1,5 @@
 import Textarea from "react-textarea-autosize";
-import { AiOutlineEnter } from "react-icons/ai";
-import { Button } from "@/components/Button";
+import Button from "@/components/Button";
 
 export default function ChatInput({ input, setInput, handleSubmit }) {
   return (
@@ -11,15 +10,14 @@ export default function ChatInput({ input, setInput, handleSubmit }) {
       <div className="w-full max-w-2xl items-center px-6">
         <div className="relative flex w-full flex-col items-start gap-2">
           <div className="relative flex w-full items-center">
-            <Textarea
+            <textarea
               name="message"
               rows={1}
-              maxRows={5}
-              tabIndex={0}
+              maxLength={200} // Optional: limit the number of characters
               placeholder="Try asking me something!"
               spellCheck={false}
               value={input}
-              className="min-h-12 w-full resize-none rounded-[28px] border border-input bg-muted pb-1 pl-4 pr-10 pt-3 text-sm shadow-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-nvidia focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50"
+              className="min-h-16 w-full resize-none rounded-[28px] border border-input bg-muted pb-1 pl-4 pr-10 pt-3 text-sm shadow-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-nvidia focus-visible:ring-offset-0"
               onChange={(e) => setInput(e.target.value)}
               onKeyDown={(e) => {
                 if (
@@ -34,15 +32,7 @@ export default function ChatInput({ input, setInput, handleSubmit }) {
                 }
               }}
             />
-            <Button
-              type="submit"
-              size="icon"
-              variant="ghost"
-              className="absolute right-2 top-1/2 mr-1 -translate-y-1/2 transform"
-              disabled={input.length === 0}
-            >
-              <AiOutlineEnter size={20} />
-            </Button>
+            <Button disabled={input.length === 0} />
           </div>
         </div>
         <p className="p-2 text-center text-xs text-zinc-400">
