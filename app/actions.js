e9b736f55bbd95abb6ca2afa00304051b8c20b4a
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2025-04-01 08:56:09 +0100

    simplified ai instance; removed rate limit checker

--- app/actions.js
+++ app/actions.js
@@ -3,8 +3,6 @@
 import { createStreamableValue } from "ai/rsc";
 import { streamText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
-import { rateLimit } from "@/lib/ratelimit";
-import { headers } from "next/headers";
 
 const nim = createOpenAI({
   baseURL: "https://integrate.api.nvidia.com/v1",
@@ -12,15 +10,6 @@ const nim = createOpenAI({
 });
 
 export async function continueConversation(messages) {
-  const headersList = await headers(); // Await the headers() function
-  const ip = headersList.get("x-forwarded-for") ?? "unknown";
-  const isRateLimited = rateLimit(ip);
-
-  if (isRateLimited) {
-    console.log("Rate limited");
-    throw new Error(`Rate Limit Exceeded for ${ip}`);
-  }
-
   const result = await streamText({
     model: nim("google/gemma-2-9b-it"),
     messages,
