259053aeedd73ba608d10a01e1fe9e33dff48915
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2025-04-11 03:15:07 +0100

    removed unneeded divs from chatinput comp

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -9,21 +9,17 @@ export default function ChatInput({
   return (
     <form
       onSubmit={handleSubmit}
-      className="fixed bottom-0 left-0 right-0 flex justify-center dark:from-background mb-5"
+      className="fixed bottom-0 left-0 right-0 flex justify-center mb-5 px-6"
     >
-      <div className="w-full max-w-2xl items-center px-6">
-        <div className="relative flex w-full flex-col items-start gap-2">
-          <div className="relative flex w-full items-center">
-            <textarea
-              placeholder="Ask me about anything!"
-              value={input}
-              className="min-h-16 w-full resize-none rounded-[28px] border border-input bg-muted pb-1 pl-4 pr-10 pt-3 text-sm shadow-sm ring-offset-background file:bg-transparent placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-nvidia focus-visible:ring-offset-0"
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
-            <Button disabled={input.length === 0} />
-          </div>
-        </div>
+      <div className="relative w-full max-w-2xl">
+        <textarea
+          placeholder="Ask me about anything!"
+          value={input}
+          className="min-h-16 w-full resize-none rounded-[28px] border border-input bg-muted pb-1 pl-4 pr-10 pt-3 text-sm shadow-sm ring-offset-background file:bg-transparent placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-nvidia focus-visible:ring-offset-0"
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <Button disabled={input.length === 0} />
       </div>
     </form>
   );
